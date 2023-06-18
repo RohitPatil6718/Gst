67845d56a0abba2ca4136a1083552d6aff767419
Rohitpatil6718 <rohitpatil6718@gmail.com>
2023-06-18 13:33:18 +0530

    welcome again

--- dd1/src/dd1/class2.java
+++ dd1/src/dd1/class2.java
@@ -5,7 +5,7 @@ public class class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("welcome rohit");
+		System.out.println("welcome rohit hi rohi welcome rohit");
 
 	}
 
